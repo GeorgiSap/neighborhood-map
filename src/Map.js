c0d84a7ce92fdcb0ed78fcb86932acbae0056181
GeorgiSap <georgisapundzhiev@hotmail.com>
2018-07-21 02:45:56 +0300

    Displayed markers

--- src/Map.js
+++ src/Map.js
@@ -1,29 +1,32 @@
 import React, { Component } from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps";
 
 class Map extends Component {
 
+	render() {
+		return (
+	      	<GoogleMap
+		        defaultZoom={11} 
+			    defaultCenter={{
+			      lat: 42.133322,
+			      lng: 23.5499485 
+			    }}
+			    defaultMapTypeId='terrain'
+			    clickableIcons={true}
+			    options={{ mapTypeControl: false }}
+	  		>
 
-render() {
-	return (
-      	<GoogleMap
-	        defaultZoom={11} 
-		    defaultCenter={{
-		      lat: 42.133322,
-		      lng: 23.5499485 
-		    }}
-		    defaultMapTypeId='terrain'
-		    clickableIcons={true}
-		    options={{ mapTypeControl: false }}
-  		/>
-	)
-}
+	  		{this.props.locations.map((location, index) => (
+	  			<Marker
+	  				key={index}
+	  				title={location.title}
+	  				position={location.position}
+	  			/>
+			))}
 
-
-
-
-
-
+	  		</GoogleMap>
+		)
+	}
 }
 
 export default withScriptjs(withGoogleMap(Map));

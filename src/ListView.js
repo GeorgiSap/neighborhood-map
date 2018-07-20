6d96b6a0b87f77c38ae614bf496dd6a42e4e7262
GeorgiSap <georgisapundzhiev@hotmail.com>
2018-07-20 19:03:39 +0300

    Added style to ListView

--- src/ListView.js
+++ src/ListView.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import './ListView.css';
 
 class ListView extends Component {
 
 	render() {
 		return (
-			<aside>
-				<input type="text" placeholder="Search..."/>
-				<ul>
+			<aside className="list-section">
+				<input className="search-input" type="text" placeholder="Search..."/>
+				<ul className="list-view">
 					{this.props.locations.map((location, index) => (
-						<li key={index}>{location.title}</li>
+						<li className="list-item" key={index}>{location.title}</li>
 					))}
 				</ul>
 			</aside>

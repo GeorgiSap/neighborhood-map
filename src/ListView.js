a7480d1f1bd07a851ce99a1a390eb5434a2621e3
GeorgiSap <georgisapundzhiev@hotmail.com>
2018-07-22 15:10:41 +0300

    Implemented search by location title

--- src/ListView.js
+++ src/ListView.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 import './ListView.css';
+import escapeRegExp from 'escape-string-regexp';
 
 class ListView extends Component {
 
+	state = {
+		query: ''
+	}
+
+	updateQuery = query => {
+		this.setState({query})
+	}	
+
 	render() {
+		let showingLocations
+		if (this.state.query) {
+			const match = new RegExp(escapeRegExp(this.state.query), 'i')
+			showingLocations = this.props.locations.filter(location => 
+				match.test(location.title))
+		} else {
+			showingLocations = this.props.locations
+		}
+
 		let asideClassList;
 		if (this.props.isListViewOpened) {
 			asideClassList = 'open'
@@ -13,9 +31,14 @@ class ListView extends Component {
 
 		return (
 			<aside className={asideClassList}>
-				<input className="search-input" type="text" placeholder="Search..."/>
+				<input 
+				 className="search-input"
+				 type="text" 
+				 placeholder="Search..."
+				 value={this.state.query}
+				 onChange={event => this.updateQuery(event.target.value) } />
 				<ul className="list-view">
-					{this.props.locations.map((location, index) => (
+					{showingLocations.map((location, index) => (
 						<li className="list-item" key={index}>
 							<a onClick={() => this.props.openInfoWindow(location)} >
 								{location.title} 

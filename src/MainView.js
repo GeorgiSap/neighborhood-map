f8fe8f6e2f52baac86faf3acc5edb728381fb600
GeorgiSap <georgisapundzhiev@hotmail.com>
2018-07-22 21:27:09 +0300

    Added aria attributes to map

--- src/MainView.js
+++ src/MainView.js
@@ -22,7 +22,7 @@ class MainView extends Component {
 			    	openInfoWindow={this.props.openInfoWindow}
 			    	selectedLocation={this.props.selectedLocation}
 				    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
-			      	containerElement={<div style={{height: `calc(100% - 3em)`, width: `100%`, position: `relative`, textAlign: `center` }} />}
+			      	containerElement={<div aria-label="location" role="application" style={{height: `calc(100% - 3em)`, width: `100%`, position: `relative`, textAlign: `center` }} />}
   					mapElement={<div style={{ position: `absolute`, height: `100%`, width: `100%`}} />}
 			      	loadingElement={<div style={{ height: `100%` }}/>}
 				/>

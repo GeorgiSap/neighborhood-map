c1bc717d3c4d0f3ac59c1733a0af41b1be3f9850
GeorgiSap <georgisapundzhiev@hotmail.com>
2018-07-23 20:55:51 +0300

    Exported locations data as json

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import ListView from './ListView.js';
 import MainView from './MainView.js';
+import * as locationsData from './data/locations.json';
 
 class App extends Component {
 
@@ -9,18 +10,7 @@ class App extends Component {
     isListViewAlongside : false,
     isListViewOpened : false,
     isMainDarkened : false,
-    locations : [
-      {title: 'Musala Peak', position : {lat : 42.179243, lng : 23.5852432}},
-      {title: 'Rila Monastery', position : {lat : 42.1333838, lng : 23.3401215}},
-      {title: 'Malyovitsa Peak', position : {lat : 42.1666667, lng : 23.3666667}},
-      {title: 'Seven Rila Lakes', position : {lat : 42.202778, lng : 23.32}},
-      {title: 'Yazovir Belmeken', position : {lat : 42.1666667, lng : 23.8}},
-      {title: 'Skakavitsa Waterfall', position : {lat : 42.2213501, lng : 23.3059478}}, 
-      {title: 'Parangalitsa', position : {lat : 42.0057878, lng : 23.3622739}},
-      {title: 'Waterfall Kostenets', position : {lat : 42.2485196, lng : 23.8048843}},
-      {title: 'Markudjik Ski Center', position : {lat : 42.2157817, lng : 23.5756325}},
-      {title: 'Carska Bistrica Residence', position : {lat : 42.2584737, lng : 23.5953557}}
-    ],
+    locations : locationsData.locations,
     selectedLocation : false
   }
 
@@ -128,6 +118,7 @@ class App extends Component {
   }
 
   componentDidMount() {
+
     this.setViewStateOnMount()
     this.setViewStateOnResize()
     this.fetchElevationData()
